ac137fa6e5b80a91adbe3c4ec9d9bde642d8399b
sher ali shahid <127499729+sherubalti@users.noreply.github.com>
2026-01-24 20:55:38 +0500

    Update examSchedule.js

--- src/data/examSchedule.js
+++ src/data/examSchedule.js
@@ -1,8 +1,8 @@
 
 const examSchedule = {
- startDate: "2026-01-24T10:00",
- endDate: "2026-01-25T18:00",
- duration: 60 // minutes
+ startDate: "2026-01-24T20:57",
+ endDate: "2026-01-25T21:00",
+ duration: 3 // minutes
 };
 
 export default examSchedule;
