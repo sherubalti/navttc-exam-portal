05d7be4e4927fe090ab8e129c0e6433ed67d8963
sher ali shahid <127499729+sherubalti@users.noreply.github.com>
2026-05-08 15:40:34 +0500

    Update examSchedule.js

--- src/data/examSchedule.js
+++ src/data/examSchedule.js
@@ -3,8 +3,8 @@ const examSchedule = {
  slots: [
 
   { start: "2026-05-08T15:40", end: "2026-05-08T15:50", course: "new_ai" },
-  { start: "2026-01-26T10:15", end: "2026-01-26T11:15", course: "old_ai" },
-  { start: "2026-01-26T13:10", end: "2026-01-26T14:10", course: "web" },
+  { start: "2026-05-08T15:40", end: "2026-05-08T15:50", course: "old_ai" },
+  { start: "2026-05-08T15:40", end: "2026-05-08T15:50", course: "web" },
 
   
   { start: "2026-01-26T14:40", end: "2026-01-26T15:40", course: "new_ai" },
